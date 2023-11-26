cffd0364b7dfd85b41bdeb806bd6cca8aa7c2921
Ankit Kumar <ankit.kumar@kankit.es>
2023-11-26 20:20:28 +0100

    Se ha añadido pagina registro de agentes

--- Afiliado/Components/Dto/Afiliado.cs
+++ Afiliado/Components/Dto/Afiliado.cs
@@ -2,30 +2,61 @@
 {
     public class Afiliado
     {
-        public required string Username { get; set; }
-        public required string Img { get; set; }
-        public required DateTime FechaRegistro { get; set; }
-        public required decimal Gastos { get; set; }
-        public decimal CincoPorCientoGastos => Gastos * 0.05m;
+        public string Username { get; set; }
+        public string Img { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public decimal Gastos { get; set; }
+        public decimal Ganancia { get; }
+
+        public Afiliado(string username, string img, DateTime fechaRegistro, decimal gastos)
+        {
+            Username = username;
+            Img = img;
+            FechaRegistro = fechaRegistro;
+            Gastos = gastos;
+            Ganancia = gastos * 0.05m;
+        }
     }
 
     public static class AfiliadoList
     {
-        public static List<Afiliado> GetColors()
+        public static List<Afiliado> GetAfiliados()
         {
             return new List<Afiliado>
             {
-                new Afiliado { Username = "user123", Img = "img/user123.jpg", FechaRegistro = new DateTime(2022, 5, 10), Gastos = 500 },
-                new Afiliado { Username = "johndoe", Img = "img/johndoe.jpg", FechaRegistro = new DateTime(2023, 1, 15), Gastos = 1200 },
-                new Afiliado { Username = "alice95", Img = "img/alice95.jpg", FechaRegistro = new DateTime(2023, 7, 20), Gastos = 800 },
-                new Afiliado { Username = "testuser", Img = "img/testuser.jpg", FechaRegistro = new DateTime(2023, 10, 5), Gastos = 300 },
-                new Afiliado { Username = "coolgamer", Img = "img/coolgamer.jpg", FechaRegistro = new DateTime(2022, 11, 30), Gastos = 1500 },
-                new Afiliado { Username = "newuser", Img = "img/newuser.jpg", FechaRegistro = new DateTime(2023, 3, 18), Gastos = 700 },
-                new Afiliado { Username = "happycoder", Img = "img/happycoder.jpg", FechaRegistro = new DateTime(2022, 8, 25), Gastos = 2000 },
-                new Afiliado { Username = "booklover", Img = "img/booklover.jpg", FechaRegistro = new DateTime(2023, 5, 9), Gastos = 100 },
-                new Afiliado { Username = "travelbug", Img = "img/travelbug.jpg", FechaRegistro = new DateTime(2022, 12, 12), Gastos = 900 },
-                new Afiliado { Username = "fitnessfreak", Img = "img/fitnessfreak.jpg", FechaRegistro = new DateTime(2023, 8, 7), Gastos = 1300 },
+                new Afiliado ("user123", "./img/Persona.jpg", new DateTime(2022, 5, 10), 500 ),
+                new Afiliado ("johndoe", "./img/Persona (10).jpeg", new DateTime(2023, 1, 15), 1200),
+                new Afiliado ("alice95", "./img/Persona (2).jpg", new DateTime(2023, 7, 20), 800),
+                new Afiliado ("testuser", "./img/Persona (3).jpg", new DateTime(2023, 10, 5), 300),
+                new Afiliado ("coolgamer", "./img/Persona (4).jpg", new DateTime(2022, 11, 30), 1500),
+                new Afiliado ("newuser", "./img/Persona (5).jpg", new DateTime(2023, 3, 18), 700),
+                new Afiliado ("happycoder", "./img/Persona (6).jpg", new DateTime(2022, 8, 25), 2000),
+                new Afiliado ("booklover", "./img/Persona (7).jpg", new DateTime(2023, 5, 9), 100),
+                new Afiliado ("travelbug", "./img/Persona (8).jpg", new DateTime(2022, 12, 12), 900),
+                new Afiliado ("fitnessfreak", "./img/Persona (9).jpg", new DateTime(2023, 8, 7), 1300),
             };
+        }
+
+        public static List<Afiliado> OrdenarPorGastosDeMayorAMenor()
+        {
+            return GetAfiliados().OrderByDescending(a => a.Gastos).ToList();
+        }
+
+        public static List<Afiliado> OrdenarPorGananciaDeMayorAMenor()
+        {
+            return GetAfiliados().OrderByDescending(a => a.Ganancia).ToList();
+        }
+
+        public static List<Afiliado> OrdenarPorFechaDeRegistroMasNuevos()
+        {
+            return GetAfiliados().OrderByDescending(a => a.FechaRegistro).ToList();
+        }
+
+        public static decimal CalcularSumaTotalGanancias()
+        {
+            var afiliados = GetAfiliados();
+            decimal sumaTotal = afiliados.Sum(a => a.Ganancia);
+            return sumaTotal;
         }
     }
 }
--- Afiliado/Components/Dto/NavItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Afiliado.Components.Dto
-{
-    public class NavItem
-    {
-        public string Text { get; set; }
-        public string Url { get; set; }
-    }
-}
